9b62e109bf6f31dbcf2bc76f40d5541eca7f7419
jamila-amigoscode <154538581+jamila-amigoscode@users.noreply.github.com>
2024-01-01 20:39:49 +0000

    improve syntax

--- merge-conficts.js
+++ merge-conficts.js
@@ -1,13 +1,9 @@
 // this function adds 2 nums
-const add = (n1, n2) => {
-  return n1 + n2;
-};
+const add = (n1, n2) => n1 + n2;
 
-const subtract = (n1, n2) => {
-  return n1 - n2
-}
+const subtract = (n1, n2) => n1 - n2;
 
 export {
   add,
   subtract
-}
+};
