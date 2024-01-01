24b34bc4c8e1d272f47d4b652111f6803d53a357
jamila-amigoscode <154538581+jamila-amigoscode@users.noreply.github.com>
2024-01-01 20:57:37 +0000

    Merge conflicts (#8)
    * added merge-conficts.js
    
    * Fixed add function and added subtract function
    
    * add back semicolin
    
    * rename minus to subtract
    
    * export add and sub
    
    * fix: export correct function
    
    * improve syntax
    
    * add divide function
    
    * improve devide func and export it
    
    * fix typo
    
    * Update a.txt
    
    ---------
    
    Co-authored-by: amigoscode <amigoscode@gmail.com>

---
+++ merge-conficts.js
@@ -0,0 +1,17 @@
+// this function adds 2 nums
+const add = (n1, n2) => n1 + n2;
+
+const subtract = (n1, n2) => n1 - n2;
+
+const divide = (n1, n2) => {
+  if (n2 === 0) {
+    throw Error("cannot devide by 0");
+  }
+  return n1 / n2;
+}
+
+export {
+  add,
+  subtract,
+  divide
+};
