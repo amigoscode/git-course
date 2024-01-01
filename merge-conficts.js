d29d4c5dffe7423a15ea9b2c18a52f1777f70a33
jamila-amigoscode <154538581+jamila-amigoscode@users.noreply.github.com>
2024-01-01 20:45:58 +0000

    improve devide func and export it

--- merge-conficts.js
+++ merge-conficts.js
@@ -4,10 +4,14 @@ const add = (n1, n2) => n1 + n2;
 const subtract = (n1, n2) => n1 - n2;
 
 const divide = (n1, n2) => {
+  if (n2 === 0) {
+    throw Error("cannot devide by 0");
+  }
   return n1 / n2;
 }
 
 export {
   add,
-  subtract
+  subtract,
+  divde
 };
